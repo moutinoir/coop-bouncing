a05e7d9f2b0c711ccb48518fb9615ae1329ba176
pauline <machabert.pauline@gmail.com>
2015-01-24 11:30:40 +0100

    created the ball in gameplay scene the ball has a collider (is trigger true) and a rigid body the players have a collider (is trigger false) on collision a script on the ball's body CBBouncingMotion.cs logs the collision in the console in the function OnTriggerEnter

---
+++ coop-bouncing/Assets/coop_bouncing/scripts/CBBouncingMotion.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CBBouncingMotion : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other) 
+	{
+		Debug.Log("[GAMEPLAY] " + transform.parent.name + ":" + name + " collided with " 
+		          + other.transform.parent.name + ":" +  other.name);
+	}
+}
