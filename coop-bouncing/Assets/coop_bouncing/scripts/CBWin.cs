e479bf147f60d78166ac0d0ab562894da565c88d
pauline <machabert.pauline@gmail.com>
2015-01-25 09:15:59 +0100

    1st version :D

---
+++ coop-bouncing/Assets/coop_bouncing/scripts/CBWin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CBWin : MonoBehaviour 
+{
+	public CBAutoFollowSpline EnemyAutoFollowSpline;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.name == "Ball"
+		   || other.gameObject.name == "ball2D")
+		{
+			EnemyAutoFollowSpline.Speed = 0f;
+			Debug.Log("Win!");
+			Invoke("Reload", 5f);
+		}
+	}
+
+	void Reload ()
+	{
+		Application.LoadLevel(0);
+	}
+}
