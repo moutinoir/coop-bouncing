07fa4ca12f176b4d34c65c028a920de7b6ad7389
Liam <mail@liam-conroy.com>
2015-02-13 17:55:21 +0900

    start of plant tests scene

---
+++ coop-bouncing/Assets/RandomPlantTypeOne.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// http://corexii.com/ocean_insight.png as inspiration
+// This plant will represent the small clump of grass without any branching
+// Other types should be:
+
+// "Spiky Ball" plant
+// Branching Seaweed plant
+
+// From http://corexii.com/jungle.jpg
+//
+// Draping vines
+// Standing Grass
+// Branching Tree
+
+
+public class RandomPlantTypeOne : MonoBehaviour {
+
+	public int minBranches = 6;
+	public int maxBranches = 10;
+
+	public int maxPlantRadius = 20;
+	public int minPlantRadius = 10;
+
+	public Vector2[] branches;
+
+	public Vector2 RandomOnUnitCircle2(float radius) 
+	{
+		Vector2 randomPointOnCircle = new Vector2(-1,-1);
+
+		while(randomPointOnCircle.x < 0 && randomPointOnCircle.y < 0)
+			randomPointOnCircle = Random.insideUnitCircle;
+
+		randomPointOnCircle.Normalize();
+		randomPointOnCircle *= radius;
+		return randomPointOnCircle;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		int numBranches = Random.Range (minBranches, maxBranches);
+		branches = new Vector2[numBranches*2];
+
+		for (int i = 0; i < numBranches*2; i+=2) 
+		{
+			int branchLength = Random.Range(minPlantRadius, maxPlantRadius);
+
+			branches[i]   = Vector2.zero;
+			branches[i+1] = RandomOnUnitCircle2(branchLength);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		for (int i = 0; i < branches.Length; i+=2) 
+		{
+			Debug.DrawLine(branches[i],  branches[i+1]);
+		}
+	}
+}
--- coop-bouncing/Assets/bgMeshCreator.cs
+++ coop-bouncing/Assets/bgMeshCreator.cs
@@ -5,9 +5,6 @@ public class bgMeshCreator : MonoBehaviour {
 
 	public CurvySpline topSpline;
 	public CurvySpline bottomSpline;
-	
-	public float width = 50;
-	public float height = 50;
 
 	// Use this for initialization
 	void Start () 
@@ -16,32 +13,10 @@ public class bgMeshCreator : MonoBehaviour {
 		Mesh mesh = new Mesh ();
 		mf.mesh = mesh;
 
-		Debug.Log (topSpline.ControlPoints.Count);
-		Debug.Log (bottomSpline.ControlPoints.Count);
-
-
+		// really, we should probably just force this to be the same
 		int numControlPoints = Mathf.Min (bottomSpline.ControlPoints.Count, topSpline.ControlPoints.Count);
+
 		// Vertices
-
-		/*{
-			bottomSpline.ControlPoints[0].Position,
-			bottomSpline.ControlPoints[1].Position,
-			topSpline.ControlPoints[0].Position,
-			topSpline.ControlPoints[1].Position,
-			bottomSpline.ControlPoints[2].Position,
-			bottomSpline.ControlPoints[3].Position,
-			topSpline.ControlPoints[2].Position,
-			topSpline.ControlPoints[3].Position,
-			bottomSpline.ControlPoints[0].Position,
-			bottomSpline.ControlPoints[1].Position,
-			topSpline.ControlPoints[0].Position,
-			topSpline.ControlPoints[1].Position,
-			bottomSpline.ControlPoints[2].Position,
-			bottomSpline.ControlPoints[3].Position,
-			topSpline.ControlPoints[2].Position,
-			topSpline.ControlPoints[3].Position
-		};*/
-
 		Vector3[] vertices = new Vector3[numControlPoints * 2];
 
 		int arrayWalk = 0;
@@ -68,27 +43,6 @@ public class bgMeshCreator : MonoBehaviour {
 			tri[arrayWalk++] = i+3;
 			tri[arrayWalk++] = i+2;
 		}
-
-		// Triangles
-		/*
-		tri [0] = 0;
-		tri [1] = 2;
-		tri [2] = 1; //
-		tri [3] = 2;
-		tri [4] = 3;
-		tri [5] = 1; //
-		tri [6] = 4;
-		tri [7] = 1;
-		tri [8] = 3; //
-		tri [9] = 3;
-		tri [10] = 6;
-		tri [11] = 4; //
-		tri [12] = 4;
-		tri [13] = 6;
-		tri [14] = 5; //
-		tri [15] = 6;
-		tri [16] = 7;
-		tri [17] = 5;*/
 
 		// Normals
 		Vector3[] normals = new Vector3[numControlPoints * 2];
