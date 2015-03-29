56a43aa02a3abe1449789c0ee04f0a807b9fe276
Liam Conroy <mail@liam-conroy.com>
2015-03-29 11:11:07 +0900

    simple parabola curve implementation

--- coop-bouncing/Assets/liam_to_be_sorted/strude/scripts/StrudeMeshCreate.cs
+++ coop-bouncing/Assets/liam_to_be_sorted/strude/scripts/StrudeMeshCreate.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+// TODO: Fix maths so it generates out in the right direction properly
+// TODO: Add simple rules to make a sane cave
+// TODO: Regenerate out the wall meshes too
+// TODO: Fix end condition (don't end for the moment)
+// TODO: Simple scene management:
+// // Delete points as you go along
+// // Delete out vegetation and trees as you go along
+// TODO: Get out all Debug.Logs to improve performance
+
+
 //to support editing spline and seeing generated geomtry update in realtime..
 //we make script execute in edit mode.
 [ExecuteInEditMode()]
